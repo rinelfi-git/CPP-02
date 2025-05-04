3072516de8ec697faab13d2661e86d2689efcf05
erijania <erijania@student.42antananarivo.mg>
2025-05-04 12:29:27 +0300

    ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -1,10 +1,21 @@
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
 const int Fixed::bit = 8;
 
 Fixed::Fixed(void): _raw(0) {
     std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int nbr) {
+    std::cout << "Int constructor called" << std::endl; 
+    _raw = nbr << Fixed::bit;
+}
+
+Fixed::Fixed(const float nbr) {
+    std::cout << "Float constructor called" << std::endl; 
+    _raw = (int)roundf(nbr * (float)(1 << Fixed::bit));
 }
 
 Fixed::~Fixed() {
@@ -24,6 +35,14 @@ Fixed &Fixed::operator=(const Fixed& object) {
     return (*this);
 }
 
+int	Fixed::toInt(void) const {
+    return (_raw >> Fixed::bit);
+}
+
+float   Fixed::toFloat(void) const {
+    return static_cast<float>(_raw) / static_cast<float>(1 << Fixed::bit);
+}
+
 int Fixed::getRawBits(void) const {
     std::cout << "getRawBits member function called" << std::endl;
     return _raw;
@@ -31,4 +50,10 @@ int Fixed::getRawBits(void) const {
 
 void Fixed::setRawBit(const int raw) {
     _raw = raw;
+}
+
+std::ostream&   operator<<(std::ostream& stream, const Fixed& instance)
+{
+    stream << instance.toFloat();
+    return (stream);
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -1,17 +1,25 @@
 #pragma once
 #ifndef _FIXED_H_
 # define _FIXED_H_
+# include <iostream>
 
 class Fixed{
     private:
         int _raw;
         static const int bit;
     public:
-        int getRawBits(void) const;
-        void setRawBit(int const raw);
         Fixed(void);
+        Fixed(const int nbr);
+        Fixed(const float nbr);
         Fixed(const Fixed&);
         ~Fixed();
         Fixed &operator=(const Fixed&);
+
+        int     getRawBits(void) const;
+        void    setRawBit(int const raw);
+        int     toInt(void) const;
+        float   toFloat(void) const;
 };
+
+std::ostream&   operator<<(std::ostream& os, const Fixed& obj);
 #endif
